4a51eae8af335109ea8982feff8c46b42a75fdff
Dharuan Luigi <dharuanluigi@gmail.com>
2025-02-19 21:28:10 -0300

    feat(mutex): add simple raw mutex implementation

--- src/main/java/some/tests/Main.java
+++ src/main/java/some/tests/Main.java
@@ -1,7 +1,46 @@
 package some.tests;
 
+import some.tests.mutex.DeficientMutex;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args) throws InterruptedException {
+    var mutex = new DeficientMutex();
+
+    var th1 =
+        new Thread(
+            () -> {
+              mutex.lock();
+              try {
+                System.out.println("Thread 1 get lock");
+                Thread.sleep(10000);
+                System.out.println("Thread 1 leaves the lock");
+              } catch (Exception e) {
+                System.out.println(e.getMessage());
+              } finally {
+                System.out.println("Finishing leaves lock from thread 1");
+                mutex.unlock();
+              }
+            });
+
+    var th2 =
+        new Thread(
+            () -> {
+              mutex.lock();
+              try {
+                System.out.println("Thread 2 get lock");
+                Thread.sleep(2000);
+                System.out.println("Thread 2 leaves the lock");
+              } catch (Exception e) {
+                System.out.println(e.getMessage());
+              } finally {
+                System.out.println("Finishing leaves lock from thread 1");
+                mutex.unlock();
+              }
+            });
+
+    th1.start();
+    th2.start();
+    th1.join();
+    th2.join();
   }
 }
--- src/main/java/some/tests/mutex/DeficientMutex.java
+++ src/main/java/some/tests/mutex/DeficientMutex.java
@@ -1,3 +1,19 @@
 package some.tests.mutex;
 
-public class UneficientMutex {}
+public class DeficientMutex {
+
+  private volatile boolean isLocked = false;
+
+  public void lock() {
+    while (true) {
+      if (!isLocked) {
+        isLocked = true;
+        return;
+      }
+    }
+  }
+
+  public void unlock() {
+    this.isLocked = false;
+  }
+}
